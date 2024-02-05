b31fa7b94c98657e7de89ad9d2ca5d49a0d03648
sayed refaat <refaats892@gmail.com>
2024-02-05 03:39:00 +0200

    second api

---
+++ database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base,sessionmaker
+
+
+engine=create_engine('postgresql://postgres:1234@localhost/pizza_delivery',
+    echo=True
+)
+
+
+Base=declarative_base()
+
+Session=sessionmaker()
