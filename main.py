ec272c1af50914a403455d022f9273e93fc28b99
sayed refaat <refaats892@gmail.com>
2024-02-05 04:11:12 +0200

    updated jwt_authorize from swagger

--- main.py
+++ main.py
@@ -7,8 +7,60 @@ import inspect, re
 from fastapi import FastAPI
 from fastapi.routing import APIRoute
 from fastapi.openapi.utils import get_openapi
+import inspect, re
+from fastapi.openapi.utils import get_openapi
+
 
 app=FastAPI()
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+
+    openapi_schema = get_openapi(
+        title = "My Auth API",
+        version = "1.0",
+        description = "An API with an Authorize Button",
+        routes = app.routes,
+    )
+
+    openapi_schema["components"]["securitySchemes"] = {
+        "Bearer Auth": {
+            "type": "apiKey",
+            "in": "header",
+            "name": "Authorization",
+            "description": "Enter: **'Bearer &lt;JWT&gt;'**, where JWT is the access token"
+        }
+    }
+
+    # Get all routes where jwt_optional() or jwt_required
+    api_router = [route for route in app.routes if isinstance(route, APIRoute)]
+
+    for route in api_router:
+        path = getattr(route, "path")
+        endpoint = getattr(route,"endpoint")
+        methods = [method.lower() for method in getattr(route, "methods")]
+
+        for method in methods:
+            # access_token
+            if (
+                re.search("jwt_required", inspect.getsource(endpoint)) or
+                re.search("fresh_jwt_required", inspect.getsource(endpoint)) or
+                re.search("jwt_optional", inspect.getsource(endpoint))
+            ):
+                openapi_schema["paths"][path][method]["security"] = [
+                    {
+                        "Bearer Auth": []
+                    }
+                ]
+
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
+
 
 
 def custom_openapi():
